88fa562e68af25d29e96a541645042a5c7a1a7b8
MattWong-ca <matthew.yt.wong@gmail.com>
2024-03-23 13:51:54 -0400

    Add toggle UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,9 +5,17 @@ const space500 = Space_Grotesk({ subsets: ['latin'], weight: ["500"], style: ["n
 
 export default function Home() {
   return (
-    <div className="overflow-hidden h-screen">
-      <div className={`${space500.className} flex justify-center text-3xl`} style={{ paddingTop: '5vh' }}>FrameGPT</div>
-      <div className={`${space500.className} flex justify-center text-2xl`} style={{ paddingTop: '5px' }}>Make a frame that mints a Base NFT</div>
+    <div className="overflow-hidden h-screen flex flex-col items-center">
+      <div className={`${space500.className} text-3xl`} style={{ marginTop: '5vh' }}>FrameGPT</div>
+      <div className={`${space500.className} text-2xl`} style={{ marginTop: '5px' }}>Make a frame that mints a Base NFT</div>
+      <div className="w-1/4 h-8 rounded mt-5 border-2 flex" style={{ borderColor: '#7c65c1' }}>
+        <div className="w-1/2 h-full border-r-2 flex flex-col justify-center" style={{ backgroundColor: '#7c65c1', borderColor: '#7c65c1' }}>
+          <div className={`${space500.className} text-md text-center text-white`}>Make a Frame</div>
+        </div>
+        <div className="w-1/2 h-full border-r-2 flex flex-col justify-center">
+          <div className={`${space500.className} text-md text-center`} style={{ color: '#7c65c1'}}>Ask AI Docs</div>
+        </div>
+      </div>
     </div>
   );
 }
